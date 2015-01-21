4ebc57a03f92c2c8ef05e0f42492a0cd14d268d1
River <river@river.sv.cmu.local>
2015-01-21 14:08:36 -0800

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,11 +7,18 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsnode addChild:character];
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+}
+
+-(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    // touch the screen
+    [character flap];
 }
 
 // put new methods here
